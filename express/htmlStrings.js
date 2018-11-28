ecdfdf99665ab1dae3230035603f4cb5ac4e4ddc
Phil Alsford <phil.alsford@xero.com>
2018-11-29 07:40:44 +1300

    Links

--- express/htmlStrings.js
+++ express/htmlStrings.js
@@ -13,11 +13,11 @@ h1 {
 }
 label {
   display: block;
-}</style></head><body><h1>Serverless Zeit Xero SSO App</h1><img class="mountain-logo" src="/images/mountain.png"><p><a href="login"> <img class="mountain-logo" src="/images/xero_login.png"></a></p></body></html>`
+}</style></head><body><h1>Serverless Netlify Xero SSO App</h1><img class="mountain-logo" src="/images/mountain.png"><p><a href="/login"> <img class="mountain-logo" src="/images/xero_login.png"></a></p></body></html>`
 
 function appHtml(email) {
   return `
-<!DOCTYPE html><html><head><title>Serverless Zeit Xero SSO App</title><style>body {
+<!DOCTYPE html><html><head><title>Serverless Netlify Xero SSO App</title><style>body {
   width: 650px;
   margin: auto;
 }
