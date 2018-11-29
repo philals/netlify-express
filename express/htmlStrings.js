2b6c4c3ae1a212e919b6083158356b6f902f4019
Phil Alsford <phil.alsford@xero.com>
2018-11-29 14:16:01 +1300

    Routes

--- express/htmlStrings.js
+++ express/htmlStrings.js
@@ -13,7 +13,7 @@ h1 {
 }
 label {
   display: block;
-}</style></head><body><h1>Serverless Netlify Xero SSO App</h1><img class="mountain-logo" src="/images/mountain.png"><p><a href="/login"> <img class="mountain-logo" src="/images/xero_login.png"></a></p></body></html>`
+}</style></head><body><h1>Serverless Netlify Xero SSO App</h1><img class="mountain-logo" src="/images/mountain.png"><p><a href="/.netlify/functions/server/login"> <img class="mountain-logo" src="/images/xero_login.png"></a></p></body></html>`
 
 function appHtml(email) {
   return `
