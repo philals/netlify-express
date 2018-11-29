434d0a6afb5279e33204520be0314b0c89f349f9
Phil Alsford <phil.alsford@xero.com>
2018-11-30 11:16:52 +1300

    remove

--- express/server.js
+++ express/server.js
@@ -1,13 +1,16 @@
 const strings = require('./htmlStrings');
-const shared = require('./shared');
 
-const { client, makeSureDiscover, redirect } = shared;
 const { rootHtml, appHtml } = strings;
 
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+
+const { Issuer } = require('openid-client');
+
+let client;
+let redirect;
 
 const router = express.Router();
 
@@ -30,12 +33,38 @@ router.get('/callback', async (req, res) => {
 
 router.get('/login', async (req, res) => {
   await makeSureDiscover();
+  console.log('in /login and going to ', redirect);
   res.redirect('https://philalford.com/?' + redirect);
 });
 
 app.use(bodyParser.json());
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+
+async function makeSureDiscover() {
+  if (!client) {
+    let xeroIssuer = await Issuer.discover('https://integration-identity.xero-uat.com/.well-known/openid-configuration') // => Promise
+    console.log('well-known has resolved')
+    console.log('Discovered issuer %s %O', xeroIssuer.issuer, xeroIssuer.metadata);
+
+    console.log("​--------------------------------------------------------------------------")
+    console.log("​makeSureDiscover -> process.env.XERO_CLIENT_ID", process.env.XERO_CLIENT_ID)
+    console.log("​--------------------------------------------------------------------------")
+
+    client = new xeroIssuer.Client({
+      client_id: process.env.XERO_CLIENT_ID,
+      client_secret: process.env.XERO_CLIENT_SECRET
+    }); // => Client
+
+    redirect = client.authorizationUrl({
+      redirect_uri: process.env.URL + '/callback',
+      scope: 'openid email profile',
+    }); // => String (URL)
+
+    console.log("​redirectUrl: ", redirect)
+  }
+}
 
 module.exports = app;
 module.exports.handler = serverless(app);
