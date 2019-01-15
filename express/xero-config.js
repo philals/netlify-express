7f19a8bcecb0719689336c8b82a27179096dff0f
Phil Alsford <phil.alsford@xero.com>
2019-01-16 08:38:01 +1300

    Temp: change to public

--- express/xero-config.js
+++ express/xero-config.js
@@ -2,10 +2,10 @@ var b64string = process.env.OA1_PRIVATE_KEY;
 var buf = Buffer.from(b64string, 'base64');
 
 let config = {
-  "appType": "partner",
+  "appType": "public",
   "consumerKey": process.env.OA1_CONSUMER_KEY,
   "consumerSecret": process.env.OA1_CONSUMER_SECRET,
-  "privateKeyString": buf,
+  // "privateKeyString": buf,
   "callbackUrl": process.env.URL + "/.netlify/functions/server/callback-oa1"
 }
 
