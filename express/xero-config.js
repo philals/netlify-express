70918cd8bc2969977e593e5f281e8ea5e6107d91
Phil Alsford <phil.alsford@xero.com>
2019-01-17 10:39:54 +1300

    undo temp

--- express/xero-config.js
+++ express/xero-config.js
@@ -2,7 +2,7 @@ var b64string = process.env.OA1_PRIVATE_KEY;
 var buf = Buffer.from(b64string, 'base64');
 
 let config = {
-  "appType": "public",
+  "appType": "partner",
   "consumerKey": process.env.OA1_CONSUMER_KEY,
   "consumerSecret": process.env.OA1_CONSUMER_SECRET,
   // "privateKeyString": buf,
